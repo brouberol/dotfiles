0e67f813511bcd08657429068a7819a7559a645d
Balthazar Rouberol <br@imap.cc>
2019-06-08 15:38:55 +0200

    Define config for lf

--- install.sh
+++ install.sh
@@ -10,6 +10,8 @@ function install {
 if [ ! -d $HOME/.oh-my-zsh ]; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
+
+# zsh
 mkdir -p ~/.zsh
 touch ~/.zsh/export
 touch ~/.zsh/alias
@@ -17,25 +19,35 @@ touch ~/.zsh/alias
 install zsh/.zshrc ~/.zshrc
 install zsh/brouberol.zsh-theme ~/.oh-my-zsh/themes/brouberol.zsh-theme
 
+# vim
 mkdir -p ~/.vim/colors
 install vim/colors/molokai.vim ~/.vim/colors/molokai.vim
 install vim/.vimrc ~/.vimrc
 
+# git
 install git/.gitconfig ~/.gitconfig
 install git/.tigrc ~/.tigrc
 
+# ptpython
 mkdir -p ~/.ptpyhon
 install ptpython/config.py ~/.ptpython/config.py
 
+# terminator
 if [ "$(uname)" = Linux ]; then
     mkdir -p ~/.config/terminator
     install terminator/config ~/.config/terminator/config
 fi
 
+# tmux
 install tmux/.tmux.conf ~/.tmux.conf
 
+# Karabiner remappings for typematrix
 if [ "$(uname)" = Darwin ]; then
     mkdir -p ~/.config/karabiner/assets/complex_modifications
     install karabiner/karabiner.json ~/.config/karabiner/karabiner.json
     install karabiner/assets/complex_modifications/firefox.json ~/.config/karabiner/assets/complex_modifications/firefox.json
 fi
+
+# lf
+mkdir -p ~/.config/lf
+install lf/lfrc ~/.config/lf/lfrc
