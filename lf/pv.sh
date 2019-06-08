d66d55189a6c144fe8b692c9d998e99b825b497f
Balthazar Rouberol <br@imap.cc>
2019-06-08 21:22:02 +0200

    Use bat for both preview and inspect

--- lf/pv.sh
+++ lf/pv.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+filepath=$1
+
 if [ $# -eq 1 ]; then
     # executed when i is hit
-    bat --style=full --pager='less -RF' $1
-elif [ -n $(wich pistol) ]; then
-    pistol $1
+    bat --style=full --pager='less -RF' $filepath
 else
-    cat $1
+    filename=$(basename -- "$filepath")
+    extension="${filename##*.}"
+    if [ -n "$extension" ]; then
+        opts="-l $extension"
+	else
+        opts=""
+    fi
+     bat --paging=never --color=always --wrap=auto -p $opts $filepath
 fi
