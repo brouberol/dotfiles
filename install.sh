4b07ffe1077d66ea371758d5e8126930e4e6fd1e
Balthazar Rouberol <br@imap.cc>
2022-09-27 09:54:11 +0200

    Commit ipython config file for vim bindings

--- install.sh
+++ install.sh
@@ -58,3 +58,7 @@ fi
 
 # general purpose
 install inputrc ~/.inputrc
+
+# ipython
+mkdir -p ~/.ipython/profile_default
+install ipython/profile_default/ipython_config.py  ~/.ipython/profile_default/ipython_config.py
